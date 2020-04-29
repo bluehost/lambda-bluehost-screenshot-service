a274e1403d0fbeafba44ce262123fbd667259c5f
Micah Wood <micah@wpscholar.com>
2020-04-29 17:27:12 -0400

    Update deploy.js script

--- deploy.js
+++ deploy.js
@@ -2,11 +2,11 @@ const {execSync} = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
-const lambda = 'bluehost-screenshot-service';
+const lambda = path.basename(__dirname).replace('lambda-', '');
 
 if (fs.existsSync(`./${lambda}.zip`)) {
-    fs.unlinkSync(`${lambda}.zip`);
+	fs.unlinkSync(`${lambda}.zip`);
 }
 
-execSync(`zip -r ${lambda}.zip . -x "*.git*" -x "*.zip"`);
+execSync(`zip -r ${lambda}.zip . -x "*.git*" "*.zip" "deploy.js" "local.js" "package.json" "package-lock.json"`);
 execSync(`aws lambda update-function-code --function-name '${lambda}' --zip-file fileb://${lambda}.zip --profile bluehost --region=us-west-2`);
